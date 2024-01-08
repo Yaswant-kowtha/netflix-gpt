e23e677a9fc8b46682e0dfb129f139812d3c33df
Yaswanth Kowtha <61146427+Yaswant-kowtha@users.noreply.github.com>
2024-01-08 22:04:23 +0000

    Integrated TMDB API to fetch movies and play trailer on Sign In

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        Authorization:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODU1ZDg5ODI2ODY3ZjVjMGNlYTUxYzVjZjQ4NzZhNCIsInN1YiI6IjYzY2FmZjMwOWE2NDM1MDA4NzJhNjk1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kkn68r40bgQUfEtnGJUKkflMtr3wo9WIMeVFW9pqOj0',
+        accept:'application/json'
+    },
+};
